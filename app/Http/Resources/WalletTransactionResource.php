056083fe46fdeeecd698bffbdca1b49849830771
Abhishek kumar <abhishek268909@gmail.com>
2026-07-17 18:46:46 +0530

    Add WalletTransactionResource for API responses

---
+++ app/Http/Resources/WalletTransactionResource.php
@@ -0,0 +1,25 @@
+// app/Http/Resources/WalletTransactionResource.php
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class WalletTransactionResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'type' => $this->transaction_type,
+            'amount' => number_format($this->amount, 2),
+            'balance_after' => number_format($this->balance_after, 2),
+            'description' => $this->description,
+            'reference_type' => $this->reference_type,
+            'reference_id' => $this->reference_id,
+            'status' => $this->status,
+            'created_at' => $this->created_at?->toDateTimeString(),
+        ];
+    }
+}
